32635573db44677f811501e09bdcffc2b94f81a0
Rolando Fernandez <roysoft@egmail.com>
2018-09-26 17:40:41 -0400

    con get post delete ...

---
+++ poligono.js
@@ -0,0 +1,6 @@
+export default class Poligono {
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+  }
+};
--- server.js
+++ server.js
@@ -25,6 +25,30 @@ app.delete("/hijos", (req, res) => {
   res.send("DELETE method");
 });
 
+app.get('/hijos/pol/avatar', (req, res) => {
+    //let p = new Poligo
+    
+    res.send('Hello GET:/hijos/pol/avatar' );
+  })
+
+app.get('/hijos/:hijo', (req, res) => {
+    res.send('Hello ' + req.params.hijo);
+})
+
 http.createServer(app).listen(8001, () => {
   console.log("Server started at http://localhost:8001");
 });
+
+
+
+/*
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Primer servidor con Node.Js');
+});
+
+server.listen(port, host, () => {
+  console.log(`Servidor corriendo en http://${host}:${port}`);
+});
+*/
